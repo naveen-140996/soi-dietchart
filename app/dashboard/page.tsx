04a9991ca8fbf749ce2509af81f516500a00804c
Naveen Vijayan <navinbs0614@gmail.com>
2026-03-20 01:38:00 +0530

    need to change

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -95,7 +95,7 @@ export default function Dashboard() {
   return (
     <>
     <Header />
-    <div className="min-h-screen bg-gray-100 p-4 md:p-8 max-w-4xl mx-auto overflow-x-hidden">
+    <div className="min-h-screen bg-gray-100 p-2 md:p-8 w-full  overflow-x-hidden">
       
       {/* Title */}
       <h1 className="text-2xl md:text-3xl font-bold mb-6">
