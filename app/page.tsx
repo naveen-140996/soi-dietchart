bcdfc3bf37eea2f3d892137ab7fb3af2bef2a87d
Naveen Vijayan <navinbs0614@gmail.com>
2026-03-26 23:42:16 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Login from "./login/page";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-   <>
-     <Login />
-   </>
-  );
+  redirect("/login");
 }
